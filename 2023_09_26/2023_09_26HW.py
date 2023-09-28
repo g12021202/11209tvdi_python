556a95365a6cea27fe25e7f288299076bfbde51b
g12021202 <g12021202@gmail>
2023-09-28 16:28:04 +0800

    update

---
+++ 2023_09_26/2023_09_26HW.py
@@ -0,0 +1,29 @@
+import random
+PlayerDices = [random.randint(1,6) for x in range(4)]
+PlayerDices.sort()
+print(f"點數由小到大為: {PlayerDices[0]} ,  {PlayerDices[1]} ,  {PlayerDices[2]} ,  {PlayerDices[3]}")
+if PlayerDices[0] == PlayerDices[1] == PlayerDices[2] == PlayerDices[3]:
+    print("豹子")
+elif PlayerDices[0] == PlayerDices[1] == PlayerDices[2] or PlayerDices[1] == PlayerDices[2] == PlayerDices[3]:
+    print("3個相同的數字，作廢")
+elif PlayerDices[0] == PlayerDices[1] != PlayerDices[2] != PlayerDices[3] or PlayerDices[0] == PlayerDices[1] != PlayerDices[2] == PlayerDices[3]:
+    result = PlayerDices[2] + PlayerDices[3]
+    print(f"點數: {result}") 
+elif PlayerDices[0] != PlayerDices[1] == PlayerDices[2] != PlayerDices[3] :
+    result = PlayerDices[1] + PlayerDices[3]
+    print(f"點數: {result}") 
+elif PlayerDices[0] != PlayerDices[1] != PlayerDices[2] == PlayerDices[3]:
+    result = PlayerDices[0] + PlayerDices[1]
+    print(f"點數: {result}") 
+elif PlayerDices[0] != PlayerDices[1] != PlayerDices[2] != PlayerDices[3]:
+    print("4個數都不同，作廢")
+
+
+
+
+# BossDices = [random.randint(1,6) for x in range(4)]
+# BossDices.sort()
+# if BossDices[0] == BossDices[1] == BossDices[2] == BossDices[3]:
+#     pass
+# else:
+#     pass
