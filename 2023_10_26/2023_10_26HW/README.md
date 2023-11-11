b9671cb638324327f233f8ef762b1668e78ce53e
g12021202 <g12021202@g12021202s-MacBook-Pro.local>
2023-11-12 00:29:12 +0800

    modify

--- 2023_10_26/2023_10_26HW/README.md
+++ 2023_10_26/2023_10_26HW/README.md
@@ -1,3 +1,7 @@
+將下載資料與建置資料庫放置在  datasource.py
+執行介面與呼叫 datasource.py 放在 app.py
+datasource.py
+```
 import requests
 import sqlite3
 import datetime
@@ -59,3 +63,47 @@ def update_sqlite_data():
     for item in data["records"]:
         __insert_data(conn,[item['sitename'],item['siteengname'],item['areaname'],item['county'],item['township'],item['siteaddress'],item['twd97lon'],item['twd97lat'],item['sitetype'],item['siteid']])
     conn.close()
+```
+
+app.py
+```
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from threading import Timer
+import datasource
+
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        try:
+            data = datasource.download_air_data()
+        except Exception as e:
+            messagebox.showerror("錯誤",f'{e}\n將關閉應用程式\n請稍後再試')
+            self.destroy()
+        
+t = None
+def main():
+    def on_closing():
+        print("window關閉")
+        t.cancel()
+        window.destroy()
+    
+    def update_data()->None:
+        datasource.update_sqlite_data()
+        global t
+        t = Timer(5000,update_data)
+        t.start()
+
+    window = Window()
+    window.title('空氣品質監測站基本資料')
+    window.geometry('600x300')
+    window.resizable(height = False, width = False)
+    update_data()
+    window.protocol("WM_DELETE_WINDOW",on_closing)
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
+
+```
--- 2023_11_10/datasource.py
+++ 2023_11_10/datasource.py
@@ -1,8 +1,9 @@
 import requests
 import psycopg2
 import password as pw
+import datetime
 
-def download_air_data()->list[dict]:
+def __download_air_data()->list[dict]:
     '''
     下載 
     https://data.moenv.gov.tw/swagger/#/%E5%A4%A7%E6%B0%A3/get_aqx_p_07
@@ -15,31 +16,54 @@ def download_air_data()->list[dict]:
     print("下載成功")
     return response.json()
 
-def create_table(conn):    
+def __create_table(conn):    
     cursor = conn.cursor()
     cursor.execute(
         '''
-        CREATE TABLE IF NOT EXISTS 觀測站(
-            "測站編號"	INTEGER,
-            "城市"	TEXT NOT NULL,
-            "測站名稱"	TEXT NOT NULL,
-            "空品區"	TEXT NOT NULL,
-            "測站類型"	TEXT NOT NULL,
-            "測站地址"	TEXT NOT NULL,
-            "資料更新時間" DATETIME NOT NULL,
-            PRIMARY KEY(測站編號),
-            UNIQUE(城市,測站名稱) ON CONFLICT REPLACE
+        CREATE TABLE IF NOT EXISTS 空氣品質監測站基本資料 (
+            "id" SERIAL,
+            "測站名稱"	TEXT,
+            "測站英文名稱"	TEXT,
+            "空品區"	TEXT,
+            "城市"	TEXT,
+            "鄉鎮"	TEXT,
+            "測站地址"	TEXT,
+            "經度" DECIMAL,
+            "緯度" DECIMAL,
+            "測站類型"	TEXT,
+            "測站編號" INTEGER,
+            "更新時間" TEXT,
+            PRIMARY KEY("id"),
+            UNIQUE(測站名稱,更新時間)
         );
         '''
     )
-    con.commit()
-
-def insert_data(conn,values:list[any])->None:
-    cursor = conn.cursor()
-    sql = '''
-    REPLACE INTO 觀測站(測站編號, 城市, 測站名稱, 空品區, 測站類型, 測站地址, 資料更新時間)
-        VALUES(?,?,?,?,?,?,?)
-    '''
-    cursor.execute(sql,values)    
     conn.commit()
     cursor.close()
+    print("create_table成功")
+
+def __insert_data(conn,VALUES:list[any])->None:
+    cursor = conn.cursor()
+    cursor_time = datetime.datetime.utcnow()
+    sql = '''
+    INSERT INTO 空氣品質監測站基本資料 (測站名稱, 測站英文名稱, 空品區, 城市, 鄉鎮, 測站地址, 經度, 緯度, 測站類型, 測站編號, 更新時間)
+    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    ON CONFLICT (測站名稱,更新時間) DO NOTHING
+    '''
+    VALUES.append(cursor_time)
+    cursor.execute(sql, VALUES) 
+    conn.commit()
+    # cursor.close()
+
+def update_render_data()->None:
+    # 下載並更新資料庫 
+    data = __download_air_data()
+    conn = psycopg2.connect(database=pw.DATABASE,
+                            user=pw.USER, 
+                            password=pw.PASSWORD,
+                            host=pw.HOST, 
+                            port="5432")
+    __create_table(conn)
+    for item in data["records"]:
+        __insert_data(conn,[item['sitename'],item['siteengname'],item['areaname'],item['county'],item['township'],item['siteaddress'],item['twd97lon'],item['twd97lat'],item['sitetype'],item['siteid']])
+    conn.close()
