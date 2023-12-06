b429e1bb970076fbc8376aa48d279eeae881caa7
g12021202 <g12021202@gmail.com>
2023-12-06 03:34:25 +0000

    modify

---
+++ flask_dash/dash_file/dash_app2.py
@@ -0,0 +1,11 @@
+from dash import Dash, html
+import pandas as pd
+dash2 = Dash(requests_pathname_prefix="/dash/app2/")
+
+dash2.layout = html.Div(
+    [html.H1("Dash H1"),
+    html.P("這是段落1"),
+    html.P("這是段落2")],
+    className="container-lg",
+    style={''}
+    )
